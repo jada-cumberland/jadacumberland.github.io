2db9ed9eda123a19aa5a945e4b6be795f7503c9f
Jada Cumberland <182374594+jadacumberland@users.noreply.github.com>
2025-03-30 04:18:54 -0400

    Reformatted file system and removed _src from git.

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 const {DateTime} = require('luxon');
 
 module.exports = function(eleventyConfig) {
+    // Ignore .gitignore
+    eleventyConfig.setUseGitIgnore(false);
+
+    // Date config
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
 
+    // Category filter config
     eleventyConfig.addFilter('categoryFilter', function(collection, category) {
         if (!category) return collection;
         return collection.filter(item => item.data.category === category);
